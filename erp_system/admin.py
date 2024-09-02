5fb43bbdad109bb9912675b157b40a42e8bdd69c
AMinSC <cdtjdcjf@gmail.com>
2024-09-02 14:40:54 +0900

    customer user profile

--- erp_system/admin.py
+++ erp_system/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import (UserProfile
+                    , Authority
+                    , AuthorityMenu
+                    , InstallationRequest
+                    , Dealer
+                    , Client
+                    , ASRegister
+                    , History
+                    , Contractor
+                    , Combo)
 
-# Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Authority)
+admin.site.register(AuthorityMenu)
+admin.site.register(InstallationRequest)
+admin.site.register(Dealer)
+admin.site.register(Client)
+admin.site.register(ASRegister)
+admin.site.register(History)
+admin.site.register(Contractor)
+admin.site.register(Combo)
--- erp_system/models.py
+++ erp_system/models.py
@@ -1,8 +1,53 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
 
 
-class User(models.Model):
-    pass
+class UserProfileManager(BaseUserManager):
+    """ Manager for user profiles """
+    def create_user(self, email, name, password=None):
+        """ Create a new user profile """
+        if not email:
+            raise ValueError('User must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """ Create a new superuser profile """
+        user = self.create_user(email,name, password)
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """ Database model for users in the system """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    initialized = models.BooleanField(default=False)
+
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        """ Return string representation of our user """
+        return self.email
 
 
 class Authority(models.Model):
